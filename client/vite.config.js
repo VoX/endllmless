0531c8cf0a8d8f0df350c5537398926c3c4cb095
VoX <v@bitvox.me>
2025-11-20 17:54:12 -0500

    feat: add vite-plugin-singlefile to enhance build process

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 const api = process.env.OPENAI_API_KEY ? 'http://localhost:8080' : "https://endless.bitvox.me";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [preact(), viteSingleFile()],
 	server: {
 		proxy: {
 			'/wordcombine': api,
